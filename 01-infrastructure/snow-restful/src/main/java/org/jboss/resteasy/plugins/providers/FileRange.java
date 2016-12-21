43d5734e713fdcb759fe66381da94941d8c4604a
dwq <d123456789>
2016-12-21 14:44:23 +0800

    引用jboss-jax至snow-restful

---
+++ 01-infrastructure/snow-restful/src/main/java/org/jboss/resteasy/plugins/providers/FileRange.java
@@ -0,0 +1,38 @@
+package org.jboss.resteasy.plugins.providers;
+
+import java.io.File;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public class FileRange
+{
+   private File file;
+   private long begin;
+   private long end;
+
+   public FileRange(File file, long begin, long end)
+   {
+      this.file = file;
+      this.begin = begin;
+      this.end = end;
+   }
+
+   public File getFile()
+   {
+      return file;
+   }
+
+   public long getBegin()
+   {
+      return begin;
+   }
+
+   public long getEnd()
+   {
+      return end;
+   }
+
+
+}
